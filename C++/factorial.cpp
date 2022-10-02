746e8f0badf48f37b4797a6d834e229242895bf0
Syed Ashhar Rahman <92107937+ashhar-rahman@users.noreply.github.com>
2022-10-02 11:04:56 +0530

    Create factorial.cpp

---
+++ C++/factorial.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int fact(int n)
+{
+    if (n==0 || n==1)
+        return 1;
+    else
+      return n*(fact(n-1));
+}
+
+int main()
+{    
+    int n;
+    cout << "Enter the number for factorial::";
+    cin>>n;
+    
+    cout<<fact(n);
+}
