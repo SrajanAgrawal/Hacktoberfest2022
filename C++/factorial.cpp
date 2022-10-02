6ad0793f090326a693edb89640aa6688d9786e18
Mridul Sharma <91523026+Mridul07Sharma@users.noreply.github.com>
2022-10-02 20:32:29 +0530

    Create factorial.cpp

---
+++ C++/factorial.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int factorial(int n){
+    if(n==1){
+        return 1;
+    }
+    return n*factorial(n-1);
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<<factorial(n)<<endl;
+
+    return 0;
+}
