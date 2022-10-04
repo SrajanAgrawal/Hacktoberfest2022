1b4af190b3e11b478a60a73acdfe6618a7d594bd
Utkarsh Katiyar <98865912+utkarshkatiyar1@users.noreply.github.com>
2022-10-05 00:25:40 +0530

    Create Program.cpp

---
+++ C++/Program.cpp
@@ -0,0 +1,29 @@
+
+
+#include <stdio.h>
+
+int main()
+
+{
+
+	int i,j;	for(i=1;i<=4;i++)
+
+	{
+
+		for(j=5;j>=i;j--)
+
+		{
+
+			printf("@");
+
+			}
+
+			printf("\n");
+
+		}
+
+	printf("Hello World!");
+
+	return 0;
+
+}
